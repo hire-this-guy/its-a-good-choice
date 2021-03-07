5449489ea822992f7bc0c0e04b2112078d39ccae
Krzyszof Lityński <krzysztof@litynski.net>
2021-03-07 20:25:16 +0100

    Visualisation component stub

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import BpmSelector from "./components/BpmSelector/BpmSelector";
+import Visualisation from "./components/Visualisation/Visualisation";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Visualisation/>
         <BpmSelector/>
     </div>
   );
---
+++ src/components/Visualisation/Visualisation.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Visualisation.css";
+
+function Visualisation() {
+    return (
+        <div className="circle">72</div>
+    );
+}
+
+export default Visualisation;
