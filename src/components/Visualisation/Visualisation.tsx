ae85ee77174547ad20e2d7d58fb6802ebe2d3599
Krzyszof Lityński <krzysztof@litynski.net>
2021-03-09 20:20:49 +0100

    Circle animation

--- src/components/Visualisation/Visualisation.tsx
+++ src/components/Visualisation/Visualisation.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import "./Visualisation.css";
 import { useBPM } from "../../bpmProvider";
 
+
 function Visualisation() {
     const [bpm] = useBPM();
+    const style = {
+        animationDuration: `${60/bpm}s`
+    }
     return (
-        <div className="circle">{bpm}</div>
+        <div className="circle">
+            {bpm}
+            <div style={style} className="circle__shadow"/>
+        </div>
     );
 }
 
